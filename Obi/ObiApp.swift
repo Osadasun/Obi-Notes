fc5da34d9da66c1d333878aa828bcc92da229bb7
Kosuke Osada <sunlight.koh@gmail.com>
2026-03-13 00:24:02 +0900

    Initial Commit

---
+++ Obi/ObiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObiApp.swift
+//  Obi
+//
+//  Created by 長田晃輔 on 2026/03/13.
+//
+
+import SwiftUI
+
+@main
+struct ObiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ObiTests/ObiTests.swift
@@ -0,0 +1,17 @@
+//
+//  ObiTests.swift
+//  ObiTests
+//
+//  Created by 長田晃輔 on 2026/03/13.
+//
+
+import Testing
+@testable import Obi
+
+struct ObiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
